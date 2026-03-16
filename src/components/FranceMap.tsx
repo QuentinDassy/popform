737efe8b1cc88579d28ecec80f37e2dbcffe2450
Quentin <quentin.dassy@gmail.com>
2026-03-16 12:01:06 +0100

    fix: Belgique translate -26

--- src/components/FranceMap.tsx
+++ src/components/FranceMap.tsx
@@ -157,7 +157,7 @@ export default function FranceMap({
             const d = featureToPath(belgiumGeo.geometry);
             return (
               <g
-                transform="translate(0,-18)"
+                transform="translate(0,-26)"
                 style={{ cursor: "pointer" }}
                 onMouseEnter={() => setHovered("Belgique")}
                 onMouseLeave={() => setHovered(null)}
